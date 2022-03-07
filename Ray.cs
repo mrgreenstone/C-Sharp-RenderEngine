1b373ae23737a7ae667b0cf5deb21e4042927e88
Mr_Greenstone <62424095+MrGreenst0ne@users.noreply.github.com>
2022-03-07 09:59:04 +0100

    Vollständige Schnittpunktsberechnung
    Gibt ein Array an Ortsvektoren zu den Schnittpunkten wieder

--- Ray.cs
+++ Ray.cs
@@ -16,8 +16,17 @@ namespace RenderEngine {
             double a = (x0-xc) * (x0-xc) + (y0-yc) * (y0-yc) + (z0-zc) * (z0-zc) - r*r;
             double b = (x0-x1) * (x0-x1) + (y0-y1) * (y0-y1) + (z0-z1) * (z0-z1);
             double c = (x1-xc) * (x1-xc) + (y1-yc) * (y1-yc) + (z1-zc) * (z1-zc) - a - b - r * r;
-            double t0 = - c/b/2 - Math.Sqrt((c/b/2) * (c/b/2) - a/b);
-            double t1 = - c/b/2 + Math.Sqrt((c/b/2) * (c/b/2) - a/b);
+            if ((c/b/2) * (c/b/2) - a/b < 0) {
+                return new Vector[] {};
+            } else if ((c/b/2) * (c/b/2) - a/b == 0) {
+                double t = - c/b/2;
+                return new Vector[] {at(t)};
+            } else {
+                double t0 = - c/b/2 - Math.Sqrt((c/b/2) * (c/b/2) - a/b);
+                double t1 = - c/b/2 + Math.Sqrt((c/b/2) * (c/b/2) - a/b);
+                return new Vector[] {at(t0), at(t1)};
+            }
+            
             
         }
     }
